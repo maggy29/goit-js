7190de4775c1bbcbc8dfd6a50d7ab4f220638643
maggy29 <grom.m.yu@gmail.com>
2020-02-19 19:45:16 +0200

    homework-03 without task-07

---
+++ homework-03/js/task-02.js
@@ -0,0 +1,25 @@
+'use strict'
+
+const countProps = function (obj) {
+
+    let total = 0;
+
+    for (const key in obj) {
+        total += 1;
+    };
+
+    return total;
+};
+
+console.log(countProps({})); // 0
+
+console.log(countProps({
+    name: 'Mango',
+    age: 2
+})); // 2
+
+console.log(countProps({
+    mail: 'poly@mail.com',
+    isOnline: true,
+    score: 500
+})); // 3
---
+++ homework-03/js/task-03.js
@@ -0,0 +1,36 @@
+'use strict'
+
+const findBestEmployee = function (employees) {
+    let max = 0;
+    let name;
+
+    const keys = Object.keys(employees);
+
+    keys.forEach(key => {
+        if (employees[key] > max) {
+            max = employees[key];
+            name = key;
+        }
+    });
+    return name;
+};
+
+console.log(findBestEmployee({
+    ann: 29,
+    david: 35,
+    helen: 1,
+    lorence: 99
+})); // lorence
+
+console.log(findBestEmployee({
+    poly: 12,
+    mango: 17,
+    ajax: 4
+})); // mango
+
+console.log(findBestEmployee({
+    lux: 147,
+    david: 21,
+    kiwi: 19,
+    chelsy: 38
+})); // lux
